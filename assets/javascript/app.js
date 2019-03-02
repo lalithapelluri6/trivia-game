fce448c7bd1b6753b5defc4712d8b62810856836
lalitha <lalithapelluri6@gmail.com>
2019-03-01 21:35:21 -0800

    changes made to js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
     //event listeners
-    $("#time-remain").on("click", trivia.startGame);
-    $(document).on("click", ".option", trivia.quessChecker);
-})
+    $("#start").on("click", trivia.startGame);
+    $(document).on("click", ".option", trivia.guessChecker);
+});
 
-var trivia = {
+let trivia = {
     //trivia properties
     correct: 0,
     incorrect: 0,
@@ -15,31 +15,31 @@ var trivia = {
     timerId: '',
     //questions options and answers data
     questions: {
-        q1: 'Who is actually a chef?',
-        q2: 'What does Joey love to eat?',
-        q3: 'How many times has Ross been divorced?',
-        q4: 'How many types of towels does Monica have?',
-        q5: "Who stole Monica's thunder after she got engaged?",
-        q6: 'Who hates Thanks giving?',
-        q7: "Who Thinks they're always the last to find out everything?"
+        q1: 'What is the collective name for a group of lions?',
+        q2: 'What are the only two mammals that lay eggs?',
+        q3: 'What kind of animal is a komodo dragon?',
+        q4: 'Which bird has the largest wingspan of any living bird?',
+        q5: "What is the largest shark?",
+        q6: 'What type of animal is Mexican hairless?',
+        q7: "What kind of creature is a portuguese man o' war?"
     },
     options: {
-        q1: ['', '', '', ''],
-        q2: ['', '', '', ''],
-        q3: ['', '', '', ''],
-        q4: ['', '', '', ''],
-        q5: ['', '', '', ''],
-        q6: ['', '', '', ''],
-        q7: ['', '', '', '']
+        q1: ['A pride', 'A flock', 'A group', 'A pack'],
+        q2: ['cow and lion', 'tiger and fox', 'a spiny anteater and duck billed platypus', 'a blue whale and a komodo dragon'],
+        q3: ['snake', 'frog', 'lizard', 'bird'],
+        q4: ['crane', 'flamingo', 'wandering albatross', 'bald eagle'],
+        q5: ['hammer head', 'tiger shark', 'great white', 'whale shark'],
+        q6: ['a cat', 'a fox', 'a cow', 'a dog'],
+        q7: ['shark', 'whale', 'a jelly fish', 'turtle']
     },
     answers: {
-        q1: 'Monica',
-        q2: '',
-        q3: '',
-        q4: '',
-        q5: '',
-        q6: '',
-        q7: ''
+        q1: 'A pride',
+        q2: 'a spiny anteater and a duck billed platypus',
+        q3: 'lizard',
+        q4: 'wandering albatross',
+        q5: 'whale shark',
+        q6: 'a dog',
+        q7: 'a jellyfish'
     },
     //trivia methods
     startGame: function () {
@@ -53,6 +53,7 @@ var trivia = {
         $('#game').show();
 
         //empty last results
+
         $('#results').html('');
 
         //show timer
@@ -78,17 +79,17 @@ var trivia = {
             trivia.timerId = setInterval(trivia.timerRunning, 1000);
         }
 
-        var questionContent = object.values(trivia.questions)[trivia.currentSet];
+        let questionContent = Object.values(trivia.questions)[trivia.currentSet];
         $('#question').text(questionContent);
 
-        var questionOptions = object.values(trivia.options)[trivia.currentSet];
+        let questionOptions = Object.values(trivia.options)[trivia.currentSet];
 
         $.each(questionOptions, function(index, key) {
             $('#options').append($('<button class="option btn btn-info btn-lg">' +key+ '</button>'));
         })
     },
-    timeRunning : function() {
-        if(trivia.timer > -1 && trivia.currentSet < object.keys(trivia.questions).length) {
+    timerRunning : function() {
+        if(trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.questions).length) {
             $('#timer').text(trivia.timer);
             trivia.timer--;
              if(trivia.timer === 4) {
@@ -100,7 +101,56 @@ var trivia = {
             trivia.unanswered++;
             trivia.result = false;
             clearInterval(trivia.timerId);
+            resultId = setTimeout(trivia.guessResult, 1000);
+            $('#results').html('<h3>Out of time! The answer was '+ Object.values(trivia.answers)[trivia.currentSet]+'</h3>h3>');
 
         }
+        else if(trivia.currentSet === Object.keys(trivia.questions).length) {
+
+            $('#results').html('<h3>Thank you for playing!</h3>' +
+            '<p>Correct: ' + trivia.correct + '</p>' +
+            '<p>Incorrect: '+ trivia.incorrect +'</p>' +
+            '<p>Unanswered: '+ trivia.unanswered + '</p>' +
+            '<p>Please play again!</p>');
+
+            //hide game section
+            $('#game').show();
+        }
+    },
+
+    //method to evaluate option clicked
+
+    guessChecker : function() {
+
+        //timer Id for game Result
+
+        let resultId;
+
+        let currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
+
+        if ($(this).text() === currentAnswer) {
+            //turn button green for correct
+            $(this).addClass('btn-success').removeClass('btn-info');
+
+            trivia.correct++;
+            clearInterval(trivia.timerId);
+            resultId = setTimeout(trivia.guessResult, 1000);
+            $('#results').html('<h3>Correct Answer!</h3>');
+        } else {
+            $(this).addClass('btn-danger').removeClass('btn-info');
+
+            trivia.incorrect++;
+            clearInterval(trivia.timerId);
+            resultId = setTimeout(trivia.guessResult, 1000);
+            $('#results').html('<h3>Better luck next time!' + currentAnswer + '</h3>');
+        }
+    },
+        guessResult : function(){
+            trivia.currentSet++;
+          $('.option').remove();
+          $('#results h3').remove();
+
+          trivia.nextQuestion();
+
     }
-}
+};
